caa6a5c02dcea3a8df3706520e96445a9340e6f6
Taufiq Maulana <fixy.maulana@gmail.com>
2025-08-18 08:59:14 +0700

    improve SEO

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,7 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    outDir: "docs",
+  },
 });
